19c5ae5fb3bca534c364bd1a2a5025426642dcc5
lg-lg-lg <ls45t6ypi21shh@protonmail.com>
2023-01-22 16:16:30 +0500

    Fixed dependencies

--- __main__.py
+++ __main__.py
@@ -1,9 +1,6 @@
 import asyncio
 import sys
 
-### uncomment this "from..." to fix pyinstaller "missing file" bug
-### I commented this out because PyCharm removes unused import lines by default
-# from charset_normalizer import md__mypyc
 from PyQt6.QtWidgets import QApplication
 from qasync import QEventLoop
 
