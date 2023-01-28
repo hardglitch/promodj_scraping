c8d8f99151b704e096ee694b2708aca338b637b3
lg-lg-lg <ls45t6ypi21shh@protonmail.com>
2023-01-28 17:39:14 +0500

    Clean up

---
+++ main.py
@@ -0,0 +1,24 @@
+import asyncio
+import sys
+
+from PyQt6.QtWidgets import QApplication
+from qasync import QEventLoop
+
+from modules import gui
+from tests import tests
+from tests.config import Config
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    loop = QEventLoop(app)
+    asyncio.set_event_loop(loop)
+
+    try:
+        window = gui.MainWindow(loop) if not Config.DEBUG else tests.mock_main_window(loop=loop)
+        window.set_settings()
+        window.show()
+    except AttributeError:
+        sys.exit()
+
+    with loop:
+        loop.run_forever()
