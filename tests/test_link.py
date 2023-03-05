b982c0f1e2910c5e7883980a9a0efaebe92d2c0b
lg-lg-lg <ls45t6ypi21shh@protonmail.com>
2023-03-05 18:44:07 +0500

    Add Allure

--- tests/test_link.py
+++ tests/test_link.py
@@ -1,7 +1,9 @@
 from asyncio import Semaphore
+from pathlib import Path
 from typing import Set
 
 import aiofiles
+import allure
 import pytest
 from PyQt6.QtCore import pyqtSignal
 from bs4 import BeautifulSoup, ResultSet, SoupStrainer
@@ -30,104 +32,116 @@ class FakeLink(Link):
     async def _worker(self, _link: str, sem: Semaphore) -> None:
         async with sem: await self._nano_task(self._content_length)
 
+@allure.epic("Testing all link operations")
+class TestLink:
 
-@pytest.mark.asyncio
-async def test_link_init_bruteforce() -> None:
-    assert await tools.fuzzer(Link.__init__, hard_mode=debug.Switches.IS_HARD_MODE)
+    @staticmethod
+    async def _setup_resultset() -> ResultSet:
+        test_page = Path("testpage.html") if Path("testpage.html").exists() else Path("tests").joinpath("testpage.html")
+        async with aiofiles.open(test_page, "r", encoding="utf-8") as file: test_page = await file.read()
+        return BeautifulSoup(test_page, "lxml", parse_only=SoupStrainer("a")).findAll(href=True)
 
-def test_set_attribute_link() -> None:
-    assert debug.set_attribute_test(link)
+    @allure.epic("Bruteforce Instances")
+    class BruteforceInstance:
+        @allure.description("Bruteforce Link instance")
+        @pytest.mark.asyncio
+        async def test_link_init_bruteforce(self) -> None:
+            assert await tools.fuzzer(Link.__init__, hard_mode=debug.Switches.IS_HARD_MODE)
 
-@pytest.mark.asyncio
-async def test_page_init_bruteforce() -> None:
-    assert await tools.fuzzer(Page.__init__, hard_mode=debug.Switches.IS_HARD_MODE)
+        @allure.description("Bruteforce Link instance")
+        def test_set_attribute_link(self) -> None:
+            assert debug.set_attribute_test(link)
 
-def test_set_attribute_page() -> None:
-    assert debug.set_attribute_test(Page(1))
+        @allure.description("Bruteforce Page instance")
+        @pytest.mark.asyncio
+        async def test_page_init_bruteforce(self) -> None:
+            assert await tools.fuzzer(Page.__init__, hard_mode=debug.Switches.IS_HARD_MODE)
 
-# -------------------------
-
-@pytest.mark.asyncio
-async def test_filtered_found_links_positive() -> None:
-    found_links = {"1.flac", "2.flac", "2.wav","1.wav"}
-    assert isinstance(found_links, Set)
-    assert all(map(lambda x: True if type(x)==str else False, found_links))
-    assert await link._filtered_found_links(found_links) in [
-        {"1.flac", "2.flac"}, {"1.wav", "2.wav"}, {"1.flac", "2.wav"}, {"1.wav", "2.flac"}]
-
-@pytest.mark.asyncio
-async def test_filtered_found_links_legal() -> None:
-    found_links = set()
-    assert await link._filtered_found_links(found_links) == set()
-
-@pytest.mark.asyncio
-async def test_filtered_found_links_bruteforce() -> None:
-    assert await tools.fuzzer(link._filtered_found_links, hard_mode=debug.Switches.IS_HARD_MODE)
-
-# -------------------------
-
-async def _setup_resultset() -> ResultSet:
-    async with aiofiles.open("testpage.html", "r", encoding="utf-8") as file: test_page = await file.read()
-    return BeautifulSoup(test_page, "lxml", parse_only=SoupStrainer("a")).findAll(href=True)
+        @allure.description("Bruteforce Page instance")
+        def test_set_attribute_page(self) -> None:
+            assert debug.set_attribute_test(Page(1))
 
 
-@pytest.mark.asyncio
-async def test_get_filtered_links_lossless() -> None:
-    CurrentValues.is_lossless = True
-    assert link._get_filtered_links(await _setup_resultset()) == {
-        fr"https://promodj.com/source/some1.flac",
-        fr"https://promodj.com/source/some2.wav",
-        fr"https://promodj.com/source/some22.aiff",
-    }
+    @allure.epic("Full Test filtered_found_links")
+    class TestFilteredFoundLinks:
+        @pytest.mark.asyncio
+        async def test_filtered_found_links_positive(self) -> None:
+            found_links = {"1.flac", "2.flac", "2.wav","1.wav"}
+            assert isinstance(found_links, Set)
+            assert all(map(lambda x: True if type(x)==str else False, found_links))
+            assert await link._filtered_found_links(found_links) in [
+                {"1.flac", "2.flac"}, {"1.wav", "2.wav"}, {"1.flac", "2.wav"}, {"1.wav", "2.flac"}]
 
-@pytest.mark.asyncio
-async def test_get_filtered_links_lossy() -> None:
-    CurrentValues.is_lossless = True
-    assert link._get_filtered_links(await _setup_resultset()) == {
-        fr"https://promodj.com/source/some3.mp3"
-    }
+        @pytest.mark.asyncio
+        async def test_filtered_found_links_legal(self) -> None:
+            found_links = set()
+            assert await link._filtered_found_links(found_links) == set()
 
-@pytest.mark.asyncio
-async def test_get_filtered_links_lossy() -> None:
-    link_massive = ResultSet(SoupStrainer())
-    assert isinstance(link_massive, ResultSet)
-    assert link._get_filtered_links(link_massive) == set()
+        @pytest.mark.asyncio
+        async def test_filtered_found_links_bruteforce(self) -> None:
+            assert await tools.fuzzer(link._filtered_found_links, hard_mode=debug.Switches.IS_HARD_MODE)
 
-@pytest.mark.asyncio
-async def test_get_filtered_links_bruteforce() -> None:
-    assert await tools.fuzzer(link._get_filtered_links, hard_mode=debug.Switches.IS_HARD_MODE)
 
-# -------------------------
+    @allure.epic("Full Test get_filtered_links")
+    class TestGetFilteredLinks:
 
-@pytest.mark.asyncio
-async def test_get_total_filesize_by_link_list() -> None:
-    found_links = {"1", "2", "3"}
-    content_length = 100
-    assert isinstance(found_links, Set)
-    assert all(map(lambda x: True if type(x) == str else False, found_links))
+        @pytest.mark.asyncio
+        async def test_get_filtered_links_lossless(self) -> None:
+            CurrentValues.is_lossless = True
+            assert link._get_filtered_links(await TestLink._setup_resultset()) == {
+                fr"https://promodj.com/source/some1.flac",
+                fr"https://promodj.com/source/some2.wav",
+                fr"https://promodj.com/source/some22.aiff",
+            }
 
-    _link = FakeLink(message=progress[int], success=success[int], search=search[int, int], content_length=content_length)
-    await _link._get_total_filesize_by_link_list(found_links)
-    assert CurrentValues.total_size == len(found_links) * content_length
+        @pytest.mark.asyncio
+        async def test_get_filtered_links_lossy(self) -> None:
+            CurrentValues.is_lossless = True
+            assert link._get_filtered_links(await TestLink._setup_resultset()) == {
+                fr"https://promodj.com/source/some3.mp3"
+            }
 
-@pytest.mark.asyncio
-async def test_get_total_filesize_by_link_list_bruteforce() -> None:
-    assert await tools.fuzzer(link._get_total_filesize_by_link_list, hard_mode=debug.Switches.IS_HARD_MODE)
+        @pytest.mark.asyncio
+        async def test_get_filtered_links_lossy(self) -> None:
+            link_massive = ResultSet(SoupStrainer())
+            assert isinstance(link_massive, ResultSet)
+            assert link._get_filtered_links(link_massive) == set()
 
-# -------------------------
+        @pytest.mark.asyncio
+        async def test_get_filtered_links_bruteforce(self) -> None:
+            assert await tools.fuzzer(link._get_filtered_links, hard_mode=debug.Switches.IS_HARD_MODE)
 
-@pytest.mark.asyncio
-async def test_get_all_links() -> None:
-    debug.Switches.IS_PARSE.fake_func = _setup_resultset
-    debug.Switches.IS_WORKER.fake_func = _fake_worker
-    CurrentValues.is_file_history = False
-    CurrentValues.is_lossless = True
 
-    assert await link.get_all_links() == {
-        fr"https://promodj.com/source/some1.flac",
-        fr"https://promodj.com/source/some2.wav",
-        fr"https://promodj.com/source/some22.aiff",
-    }
+    @allure.epic("Full Test get_total_filesize")
+    class TestGetTotalFileSize:
+        @pytest.mark.asyncio
+        async def test_get_total_filesize_by_link_list(self) -> None:
+            found_links = {"1", "2", "3"}
+            content_length = 100
+            assert isinstance(found_links, Set)
+            assert all(map(lambda x: True if type(x) == str else False, found_links))
 
-def _fake_worker() -> None:
-    pass
+            _link = FakeLink(message=progress[int], success=success[int], search=search[int, int], content_length=content_length)
+            await _link._get_total_filesize_by_link_list(found_links)
+            assert CurrentValues.total_size == len(found_links) * content_length
+
+        @pytest.mark.asyncio
+        async def test_get_total_filesize_by_link_list_bruteforce(self) -> None:
+            assert await tools.fuzzer(link._get_total_filesize_by_link_list, hard_mode=debug.Switches.IS_HARD_MODE)
+
+
+    @pytest.mark.asyncio
+    async def test_get_all_links(self) -> None:
+        debug.Switches.IS_PARSE.fake_func = TestLink._setup_resultset
+        debug.Switches.IS_WORKER.fake_func = self._fake_worker
+        CurrentValues.is_file_history = False
+        CurrentValues.is_lossless = True
+
+        assert await link.get_all_links() == {
+            fr"https://promodj.com/source/some1.flac",
+            fr"https://promodj.com/source/some2.wav",
+            fr"https://promodj.com/source/some22.aiff",
+        }
+
+    def _fake_worker(self) -> None:
+        pass
--- tests/test_manager.py
+++ tests/test_manager.py
@@ -1,5 +1,6 @@
 from concurrent.futures import Future
 
+import allure
 import pytest
 from PyQt6.QtCore import pyqtBoundSignal
 from aiohttp import ClientSession
@@ -10,80 +11,80 @@ from modules.shared import CurrentValues
 from tests.setup import Setup
 
 
-@pytest.fixture(scope="function")
-def setup():
-    Setup()
+@allure.epic("Test the 'Manager' values and types")
+class TestManager:
+    @pytest.fixture(scope="function")
+    def setup(self):
+        Setup()
 
+    def test_default_values(self, setup):
+        mng = Manager(
+            download_dir=Data.DefaultValues.download_dir,
+            genre=Data.DefaultValues.genre,
+            form=Data.DefaultValues.form,
+            is_lossless=Data.DefaultValues.is_lossless,
+            quantity=Data.DefaultValues.quantity,
+            is_period=Data.DefaultValues.is_period,
+            threads=Data.DefaultValues.threads,
+            is_rewrite_files=Data.DefaultValues.is_rewrite_files,
+            is_file_history=Data.DefaultValues.is_file_history
+        )
+        assert CurrentValues.download_dir == Data.DefaultValues.download_dir
+        assert CurrentValues.genre == Data.DefaultValues.genre
+        assert CurrentValues.form == Data.DefaultValues.form
+        assert CurrentValues.is_lossless == Data.DefaultValues.is_lossless
+        assert CurrentValues.quantity == Data.DefaultValues.quantity
+        assert CurrentValues.is_period == Data.DefaultValues.is_period
+        assert CurrentValues.threads == Data.DefaultValues.threads
+        assert CurrentValues.is_rewrite_files == Data.DefaultValues.is_rewrite_files
+        assert CurrentValues.is_file_history == Data.DefaultValues.is_file_history
 
-def test_default_values(setup):
-    mng = Manager(
-        download_dir=Data.DefaultValues.download_dir,
-        genre=Data.DefaultValues.genre,
-        form=Data.DefaultValues.form,
-        is_lossless=Data.DefaultValues.is_lossless,
-        quantity=Data.DefaultValues.quantity,
-        is_period=Data.DefaultValues.is_period,
-        threads=Data.DefaultValues.threads,
-        is_rewrite_files=Data.DefaultValues.is_rewrite_files,
-        is_file_history=Data.DefaultValues.is_file_history
-    )
-    assert CurrentValues.download_dir == Data.DefaultValues.download_dir
-    assert CurrentValues.genre == Data.DefaultValues.genre
-    assert CurrentValues.form == Data.DefaultValues.form
-    assert CurrentValues.is_lossless == Data.DefaultValues.is_lossless
-    assert CurrentValues.quantity == Data.DefaultValues.quantity
-    assert CurrentValues.is_period == Data.DefaultValues.is_period
-    assert CurrentValues.threads == Data.DefaultValues.threads
-    assert CurrentValues.is_rewrite_files == Data.DefaultValues.is_rewrite_files
-    assert CurrentValues.is_file_history == Data.DefaultValues.is_file_history
+        assert mng._downloading is None
+        assert CurrentValues.session is None
 
-    assert mng._downloading is None
-    assert CurrentValues.session is None
+        assert mng.progress[int]
+        assert mng.success[int]
+        assert mng.search[int, int]
+        assert mng.message[str]
+        assert mng.file_info[int, int]
 
-    assert mng.progress[int]
-    assert mng.success[int]
-    assert mng.search[int, int]
-    assert mng.message[str]
-    assert mng.file_info[int, int]
+        assert CurrentValues.total_files == 0
+        assert CurrentValues.total_downloaded_files == 0
+        assert CurrentValues.total_downloaded == 0
+        assert CurrentValues.total_size == 0
 
-    assert CurrentValues.total_files == 0
-    assert CurrentValues.total_downloaded_files == 0
-    assert CurrentValues.total_downloaded == 0
-    assert CurrentValues.total_size == 0
+    def test_default_types(self, setup):
+        mng = Manager(
+            download_dir=Data.DefaultValues.download_dir,
+            genre=Data.DefaultValues.genre,
+            form=Data.DefaultValues.form,
+            is_lossless=Data.DefaultValues.is_lossless,
+            quantity=Data.DefaultValues.quantity,
+            is_period=Data.DefaultValues.is_period,
+            threads=Data.DefaultValues.threads,
+            is_rewrite_files=Data.DefaultValues.is_rewrite_files,
+            is_file_history=Data.DefaultValues.is_file_history
+        )
+        assert isinstance(CurrentValues.download_dir, str)
+        assert isinstance(CurrentValues.genre, str)
+        assert isinstance(CurrentValues.form, str)
+        assert isinstance(CurrentValues.is_lossless, bool)
+        assert isinstance(CurrentValues.quantity, int)
+        assert isinstance(CurrentValues.is_period, bool)
+        assert isinstance(CurrentValues.threads, int)
+        assert isinstance(CurrentValues.is_rewrite_files, bool)
+        assert isinstance(CurrentValues.is_file_history, bool)
 
+        assert isinstance(mng._downloading, Future | None)
+        assert isinstance(CurrentValues.session, ClientSession | None)
 
-def test_default_types(setup):
-    mng = Manager(
-        download_dir=Data.DefaultValues.download_dir,
-        genre=Data.DefaultValues.genre,
-        form=Data.DefaultValues.form,
-        is_lossless=Data.DefaultValues.is_lossless,
-        quantity=Data.DefaultValues.quantity,
-        is_period=Data.DefaultValues.is_period,
-        threads=Data.DefaultValues.threads,
-        is_rewrite_files=Data.DefaultValues.is_rewrite_files,
-        is_file_history=Data.DefaultValues.is_file_history
-    )
-    assert isinstance(CurrentValues.download_dir, str)
-    assert isinstance(CurrentValues.genre, str)
-    assert isinstance(CurrentValues.form, str)
-    assert isinstance(CurrentValues.is_lossless, bool)
-    assert isinstance(CurrentValues.quantity, int)
-    assert isinstance(CurrentValues.is_period, bool)
-    assert isinstance(CurrentValues.threads, int)
-    assert isinstance(CurrentValues.is_rewrite_files, bool)
-    assert isinstance(CurrentValues.is_file_history, bool)
+        assert isinstance(mng.progress[int], pyqtBoundSignal)
+        assert isinstance(mng.success[int], pyqtBoundSignal)
+        assert isinstance(mng.search[int, int], pyqtBoundSignal)
+        assert isinstance(mng.message[str], pyqtBoundSignal)
+        assert isinstance(mng.file_info[int, int], pyqtBoundSignal)
 
-    assert isinstance(mng._downloading, Future | None)
-    assert isinstance(CurrentValues.session, ClientSession | None)
-
-    assert isinstance(mng.progress[int], pyqtBoundSignal)
-    assert isinstance(mng.success[int], pyqtBoundSignal)
-    assert isinstance(mng.search[int, int], pyqtBoundSignal)
-    assert isinstance(mng.message[str], pyqtBoundSignal)
-    assert isinstance(mng.file_info[int, int], pyqtBoundSignal)
-
-    assert isinstance(CurrentValues.total_files, int)
-    assert isinstance(CurrentValues.total_downloaded_files, int)
-    assert isinstance(CurrentValues.total_downloaded, int)
-    assert isinstance(CurrentValues.total_size, int)
+        assert isinstance(CurrentValues.total_files, int)
+        assert isinstance(CurrentValues.total_downloaded_files, int)
+        assert isinstance(CurrentValues.total_downloaded, int)
+        assert isinstance(CurrentValues.total_size, int)
